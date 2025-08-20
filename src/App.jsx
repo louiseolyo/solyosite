da5899dc1785da84d1d2d7067c2478d54e92e1cb
Louise Durieu <louise@solyo.fr>
2025-08-20 21:20:48 +0200

    Fix: merci.jsx removed, use static merci.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import MentionsLegales from "./pages/MentionsLegales.jsx";
+import Merci from "./public/merci.html";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mentions-legales" element={<MentionsLegales />} /> 
+        <Route path="/mentions-legales" element={<MentionsLegales />} />
+        <Route path="/merci.html" element={<Merci />} />   {/* ✅ */}  
       </Routes>
     </Router>
   );
