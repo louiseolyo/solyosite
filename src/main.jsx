d11604770ba8fec34ed4796bcc3e3f3362254fb5
Louise Durieu <louise@solyo.fr>
2026-03-11 16:32:21 +0100

    update gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { hydrateRoot, createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from "react-helmet-async"
 import App from './App.jsx'
 import './index.css'
 
-const rootElement = document.getElementById('root');
-
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(rootElement, <HelmetProvider><App /></HelmetProvider>);
-} else {
-  createRoot(rootElement).render(<HelmetProvider><App /></HelmetProvider>);
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
