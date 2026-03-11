644bfaab9a47eadd4183640a3013e79b3883b715
Louise Durieu <louise@solyo.fr>
2026-03-11 16:19:12 +0100

    lien annuaire

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { HelmetProvider } from "react-helmet-async";
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { hydrateRoot, createRoot } from 'react-dom/client'
+import { HelmetProvider } from "react-helmet-async"
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <HelmetProvider>
-  <App />
-</HelmetProvider>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, <HelmetProvider><App /></HelmetProvider>);
+} else {
+  createRoot(rootElement).render(<HelmetProvider><App /></HelmetProvider>);
+}
