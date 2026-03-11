2b8a1a1e5855aa443f9234e48ac34aa752e19556
Louise Durieu <louise@solyo.fr>
2026-03-11 16:37:10 +0100

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from "react-helmet-async"
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <HelmetProvider>
     <App />
   </HelmetProvider>
